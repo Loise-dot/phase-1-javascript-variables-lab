4a86e360f9fce2088b6ec2d04b9a3fc9db87323e
Loise <loise.wambui@student.moringaschool.com>
2022-11-28 22:43:18 +0300

    Completed Assignment

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
 // Code your solution in this file!
+let companyName = "Scuber";
+companyName = "Scuber";
+let mostProfitableNeighborhood = "Chelsea";
+const companyCeo = "SusanSmith";
+js = "companyCeo";
